15b6d9f53f6f9692c62823df3cd4940e08f1147b
Zac Brown <zmb1391@gmail.com>
2015-05-14 10:56:49 -0400

    replace sequences of chars with single characters

--- module/tweet_sec.rb
+++ module/tweet_sec.rb
@@ -10,5 +10,13 @@ module TweetSec
       @password = password
     end
 
+    def random_letter
+      rand(97..122).chr
+    end
+
+    def replace_words_with_single_letter
+      password.gsub(/([a-zA-Z]{2,})/,random_letter)
+    end
+
   end
 end
--- spec/tweet_sec_spec.rb
+++ spec/tweet_sec_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../module/tweet_sec'
+require 'pry'
 
 include TweetSec
 
@@ -6,6 +7,28 @@ describe 'TweetSec' do
   describe 'evaluate' do
     it 'returns a TweetSec object' do
       expect((evaluate('')).class).to eq(TweetSec::TweetSec)
+    end
+  end
+end
+
+describe 'TweetSec::TweetSec' do
+  describe 'initialize' do
+    let(:tweet_sec) {TweetSec::TweetSec.new('test')}
+    
+    it 'stores the password' do
+      expect(tweet_sec.password).to eq('test')
+    end
+  end
+
+  describe 'replace_words_with_single_letter' do
+    it "replaces 'password' with a single letter" do
+      tweet_sec = TweetSec::TweetSec.new('password')
+      expect(tweet_sec.replace_words_with_single_letter.length).to eq(1)
+    end
+
+    it 'replaces every sequence of 2 or more chars with a single letter' do
+      tweet_sec = TweetSec::TweetSec.new('pass word password')
+      expect(tweet_sec.replace_words_with_single_letter.length).to eq(5)
     end
   end
 end
